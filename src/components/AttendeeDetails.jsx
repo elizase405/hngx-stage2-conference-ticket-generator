b861a7001e10057dcc0ffa46020a6760255cf2a8
Elizabeth <elizase405@gmail.com>
2025-02-15 08:51:34 +0100

    misc: remove axios imports

--- src/components/AttendeeDetails.jsx
+++ src/components/AttendeeDetails.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import axios from 'axios'
 import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { MdMailOutline } from "react-icons/md";
