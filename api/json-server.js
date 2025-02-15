bf412ac9b572da1961192baaf4d1d4cae56553ef
Elizabeth <elizase405@gmail.com>
2025-02-15 05:40:49 +0100

    fix: fix middleware code

--- api/json-server.js
+++ api/json-server.js
@@ -5,8 +5,13 @@ const router = jsonServer.router(path.resolve(__dirname, '../../db.json'))
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+server.use(jsonServer.rewriter({
+	"/api/*": "/$1".
+	"/blog/:resource/:id/show": ":resource/:id"
+}))
+server.listen(3000, () => {
+	console.log('JSON server is running')
+})
 server.use(router);
 
-module.exports = (req, res) => {
-	server(req, res);
-};
+module.exports = server
